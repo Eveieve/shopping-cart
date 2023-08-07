6305e1852c17261a04684dc807f48aa528132237
unknown <nhj25252@gmail.com>
2023-08-07 21:41:18 +0900

    [test] Test commit

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,34 +1,19 @@
 import { render, screen } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
+//import { describe, it, expect } from "vitest";
 import userEvent from "@testing-library/user-event";
-import App from "./App";
-
-// describe("App component", () => {
-//   it("renders correct heading", () => {
-//     render(<App />);
-//     expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
-//   });
-// });
+import App from "./App.jsx";
 
 describe("App component", () => {
   it("renders magnificent monkeys", () => {
-    // since screen does not have the container property, we'll destructure render to obtain container for this test
     const { container } = render(<App />);
-    // use 'toMatchSnapshot' to check whether all nodes render
+    // why not just toMatch()?
     expect(container).toMatchSnapshot();
   });
 
-  it("renders radical rhinos after button click", async () => {
-    // simulate a click event
-    // setting up an user event
-    const user = userEvent.setup();
+  // it("renders radical rhinos after button click", async () => {
+  //   const user = userEvent.setup();
 
-    render(<App />);
-    const button = screen.getByRole("button", { name: "Click Me" });
-
-    // simulates asynchronous user interaction
-    await user.click(button);
-
-    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
-  });
+  //   render(<App />);
+  //   const button = screen.getByRole("button", { name: "Click Me" });
+  // });
 });
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -7,6 +8,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "src/tests/setup.js", // from its point of view..src/test/setup.js
+    setupFiles: "src/tests/setup.js", // from its point of view..src/tests/setup.js
   },
 });
