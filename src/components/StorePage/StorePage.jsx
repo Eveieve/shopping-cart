ed05aea4ddc01917e36418e3163bafc4ece3037c
Mike <nhj25252@gmail.com>
2023-08-17 00:14:23 +0900

    feat: handle error

--- src/components/StorePage/StorePage.jsx
+++ src/components/StorePage/StorePage.jsx
@@ -3,15 +3,24 @@ import Items from './Items';
 
 function StorePage() {
   const [fetchedData, setFetchedData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
 useEffect(() => {
   fetch("https://fakestoreapi.com/products", {mode: "cors"})
-  .then((res) => res.json())
+  .then((res) => {
+    if(res.status>=400) {
+      throw new Error("server error");
+    }
+    return res.json()
+  })
   .then((res) => setFetchedData(res))
-  .catch((error) => console.error(error));
+  .catch((error) => setError(error))
+  .finally(() => setLoading(false)); //when it's finally resolved! 
 }, [])
 
-if (!fetchedData) return <>Loading...</>;
+if(error) return <p>A network error was encountered!</p>
+if (loading) return <>Loading...</>;
 
 
   return (
