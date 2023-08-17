4782e7ba8a1e21f3bfbb517ead650c23c2800d16
Mike <nhj25252@gmail.com>
2023-08-17 15:41:15 +0900

    style each item

--- src/components/StorePage/SingleItem.jsx
+++ src/components/StorePage/SingleItem.jsx
@@ -1,13 +1,17 @@
 function SingleItem({ imageSrc, title, price }) {
  
+  const shortenedTitle = title.slice(0,20);
+
   return (
-    <>
-      <div className="border-2 p-5">
-        <img src={imageSrc} className = "h-4/6" />
-        <p>{title}</p>
-        <p>{price}</p>
+   
+      <div className="border-4 rounded-md p-3 flex flex-col">
+        <img src={imageSrc} className = "h-4/6 m-auto" />
+        <div className="flex p-5 justify-between">
+          <p>{shortenedTitle}</p>
+          <p>${price}</p>
+        </div>
       </div>
-    </>
+    
   );
 }
 
--- src/components/StorePage/StorePage.jsx
+++ src/components/StorePage/StorePage.jsx
@@ -1,7 +1,7 @@
 import {useEffect, useState} from 'react';
 import Items from './Items';
 
-
+// custom hook 'fetchData'
 const fetchData = () => {
   const [fetchedData, setFetchedData] = useState(null);
   const [error, setError] = useState(null);
