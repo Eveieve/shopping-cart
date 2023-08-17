075847e0f4cc99a30f067971872dc02d4967e667
Mike <nhj25252@gmail.com>
2023-08-17 21:55:07 +0900

    feat: Render clicked product's ProductPAge

--- src/components/StorePage/ProductPage.jsx
+++ src/components/StorePage/ProductPage.jsx
@@ -4,11 +4,22 @@ import fetchData from "../custom-hooks/fetchData";
 function ProductPage(props) {
 
   const {routeParams} = useParams();
-  console.log(useParams()) // logs out fetchedData array! 
- 
-console.log(fetchData());
+
+  console.log(fetchData());
+const{fetchedData, error, loading} = fetchData();
+
+if(error) return <p>A network error was encountered!</p>
+if (loading) return <>Loading...</>;
+
+
+const foundProduct = fetchedData.find((item)=> item.id == routeParams);
+
   return (
+    <div>
     <h1>This is a detail page for product {routeParams}</h1>
+    <img src ={foundProduct.image}/>
+    <div>Description</div>
+    </div>
   )
 
 }
--- src/components/StorePage/SingleItem.jsx
+++ src/components/StorePage/SingleItem.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 
-function SingleItem({ imageSrc, title, price, id }) {
+function SingleItem({ imageSrc, title, price, id, fetchedData }) {
  
   const shortenedTitle = title.slice(0,20);
 
   const routeParams = id;
 
+  const data = fetchedData;
+
   return (
       // <div className="border-4 rounded-md p-3 flex flex-col">
-      <Link to = {`./${routeParams}`} className="border-4 rounded-md p-3 flex flex-col">
+      <Link data = {data} to = {`./${routeParams}`} className="border-4 rounded-md p-3 flex flex-col">
         <img src={imageSrc} className = "h-4/6 m-auto" />
         <div className="flex p-5 justify-between">
           <p>{shortenedTitle}</p>
