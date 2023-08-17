c6681dc20eac67200c1e003cf47b2cc2ff7554d5
Mike <nhj25252@gmail.com>
2023-08-18 06:17:27 +0900

    css: style product detail div to be in the center

--- src/components/StorePage/ProductPage.jsx
+++ src/components/StorePage/ProductPage.jsx
@@ -1,27 +1,25 @@
 import { useParams } from "react-router-dom";
 import fetchData from "../custom-hooks/fetchData";
 
+
 function ProductPage(props) {
 
   const {routeParams} = useParams();
 
-
-  const{fetchedData : fetchedProduct, error, loading} = fetchData(routeParams);
-
-  console.log(fetchedProduct);
-
-if(error) return <p>A network error was encountered!</p>
-if (loading) return <>Loading...</>;
+  const {fetchedData : fetchedProduct, error, loading} = fetchData(routeParams);
 
 
-//const foundProduct = fetchedProduct.find((item)=> item.id == routeParams);
+  if(error) return <p>A network error was encountered!</p>
+  if (loading) return <p>Loading...</p>;
 
   return (
-    <div className="flex basis-3/6 items-center justify-center">
-    {/* <h1>This is a detail page for product {routeParams}</h1> */}
-    <img src ={fetchedProduct.image} />
+    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center w-3/6">
+    <img src ={fetchedProduct.image} className="max-w-sm md:max-w-md"/>
     <p>${fetchedProduct.price}</p>
     <p>{fetchedProduct.description}</p>
+   
+    </div>
     </div>
   )
 
