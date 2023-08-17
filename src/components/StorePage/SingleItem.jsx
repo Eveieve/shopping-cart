2e118a89b9cdeb460f7caa1bc6a2db8aff1f835c
Mike <nhj25252@gmail.com>
2023-08-17 20:57:30 +0900

    feat: Direct to the product's detail page when clicked

--- src/components/StorePage/SingleItem.jsx
+++ src/components/StorePage/SingleItem.jsx
@@ -1,17 +1,21 @@
-function SingleItem({ imageSrc, title, price }) {
+import { Link } from "react-router-dom";
+
+function SingleItem({ imageSrc, title, price, id }) {
  
   const shortenedTitle = title.slice(0,20);
 
+
+
   return (
-   
-      <div className="border-4 rounded-md p-3 flex flex-col">
+      // <div className="border-4 rounded-md p-3 flex flex-col">
+      <Link to = {`./${id}`} className="border-4 rounded-md p-3 flex flex-col">
         <img src={imageSrc} className = "h-4/6 m-auto" />
         <div className="flex p-5 justify-between">
           <p>{shortenedTitle}</p>
           <p>${price}</p>
         </div>
-      </div>
-    
+      </Link>
+      // </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import StorePage from "./components/StorePage";
 import CartPage from "./components/CartPage";
 import Header from "./components/HomePage/Header";
 import ProductPage from "./components/StorePage/ProductPage";
-
+import SingleItem from "./components/StorePage/SingleItem";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -22,17 +22,23 @@ const router = createBrowserRouter([
         path: "/store",
         element: <StorePage />,
       },
+      
       {
         path: "/store/:id",
         element: <ProductPage />,
-        
       },
+     
       {
         path: "/cart",
         element: <CartPage />,
       },
     ]
   },
+  {
+    path: "/home",
+    element: <App />,
+  },
+
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
